410fa222071267669d1d0994760174235da2a844
Predator <Predator@DESKTOP-MO066SA>
2020-04-30 23:34:13 +0800

    First Commit

---
+++ DataAccess.Entities.Context/AAAInventoryEntities.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Entities.Context.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities.Context
+{
+    public class AAAInventoryEntities : System.Data.Entity.DbContext, IAAAInventoryEntities
+    {
+        public AAAInventoryEntities()
+            : base("name=")
+        { }
+
+    }
+}
---
+++ DataAccess.Repository.InventoryAAA/Interface/IInventoryAAARepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository.InventoryAAA.Interface
+{
+    public interface IInventoryAAARepository<T>
+    {
+        IQueryable<T> SearchFor(Expression<Func<Task, bool>> predicate);
+        IQueryable<T> GetAll();
+        T Insert(T item);
+        T Update(T item, Expression<Func<Task, bool>> predicate);
+        T Update2(T item);
+        bool Delete(Expression<Func<Task, bool>> predicate);
+        IEnumerable<T> ExecuteQuery(string commandString, params object[] param);
+        DataTable ExecuteSPReturnTable(string commandString, bool IsStoredProc, params object[] param);
+        bool RemoveRange(Expression<Func<Task, bool>> predicate);
+    }
+}
