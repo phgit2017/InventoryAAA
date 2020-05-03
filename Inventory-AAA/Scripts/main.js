22635b063c6dc90139d4a6ce494e9ba53946d7cc
kenneth-ebora <kgbebora@gmail.com>
2020-05-03 16:51:38 -0700

    Added Essential frontend components Started with Main View Design

---
+++ Inventory-AAA/Scripts/main.js
@@ -0,0 +1,23 @@
+﻿var app =
+    angular
+        .module("InventoryApp", ["ngRoute"])
+        .config(function ($routeProvider, $locationProvider) {
+            $locationProvider.hashPrefix('');
+            $routeProvider
+                .when("/", {
+                    title: "Login",
+                    controller: "HomeController",
+                    controllerAs: "homeCtrl"
+                })
+                .when("/Home", {
+                    title: "Home",
+                    templateUrl: "angular-app/home/home.html",
+                    controller: "HomeController",
+                    controllerAs: "homeCtrl"
+                })
+        })
+        .run(['$rootScope', function ($rootScope) {
+            $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+                $rootScope.title = current.$$route.title;
+            });
+        }]);
