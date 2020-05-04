bc8d6dcb9685c166629af20f42035bda5d5e380e
kenneth-ebora <kgbebora@gmail.com>
2020-05-04 03:24:21 -0700

    Added template for datatable

--- Inventory-AAA/angular-app/home/home-controller.js
+++ Inventory-AAA/angular-app/home/home-controller.js
@@ -1,27 +1,25 @@
 ﻿app.controller("HomeController", function ($scope) {
     var vm = this;
 
-    vm.Test = "test";
-
     vm.ProductList = [
         {
             Id: 1,
             Code: "MP01",
-            Descripton: "Men's Cotton Pants",
+            Description: "Men's Cotton Pants",
             CurrentStock: 30,
             Sold: 10000
         },
         {
             Id: 2,
             Code: "MP02",
-            Descripton: "Men's Silk Pants",
+            Description: "Men's Silk Pants",
             CurrentStock: 20,
             Sold: 20000
         },
         {
-            Id: 1,
+            Id: 3,
             Code: "MP03",
-            Descripton: "Men's Denim Pants",
+            Description: "Men's Denim Pants",
             CurrentStock: 10,
             Sold: 30000
         },
