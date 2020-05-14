5441a7c86a07fd46142c16eb32a5342d5bf099c4
Predator <Predator@DESKTOP-MO066SA>
2020-05-14 11:33:03 +0800

    Changes for Report and Stocks Controller

---
+++ Inventory-AAA/Infrastructure/InventoryAAAAuthorizeUserAttribute.cs
@@ -0,0 +1,46 @@
+﻿using Business.AAA.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_AAA.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class InventoryAAAAuthorizeUserAttribute : AuthorizeAttribute
+    {
+        
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            //filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary
+            //{
+            //    {"action","Index" },
+            //    {"controller","Home" }
+            //});
+
+            //filterContext.Result = new ViewResult
+            //{
+            //    ViewName = "~/Views/Unauthorized.cshtml"
+            //};
+
+            filterContext.Result = new RedirectResult("~/Views/Unauthorized.cshtml");
+            //filterContext.Result = new RedirectResult("~/#/Login");
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (this.AuthorizeCore(filterContext.HttpContext))
+            {
+                base.OnAuthorization(filterContext);
+            }
+            else
+            {
+                this.HandleUnauthorizedRequest(filterContext);
+            }
+
+            //filterContext.RequestContext.HttpContext.Response.Redirect("/#/Login", true);
+        }
+    }
+}
