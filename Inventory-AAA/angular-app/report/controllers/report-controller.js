8b05353252564840c477a9b6dcd424eb5fd599fb
Kenneth Gabrielle Ebora <kgbebora@gmail.com>
2020-10-27 16:54:23 +0800

    Added Report Enhancements

--- Inventory-AAA/angular-app/report/controllers/report-controller.js
+++ Inventory-AAA/angular-app/report/controllers/report-controller.js
@@ -2,24 +2,67 @@
     .module('InventoryApp')
     .controller('ReportController', ReportController)
 
-ReportController.$inject = ['ReportService', '$scope', '$window', '$http', 'QuickAlert', 'globalBaseUrl'];
+ReportController.$inject = ['ReportService', 'CustomerService', 'MaintenanceService', '$scope', '$window', '$http', 'QuickAlert', 'globalBaseUrl'];
 
-function ReportController(ReportService, $scope, $window, $http, QuickAlert, globalBaseUrl) {
-    var vm = this, controllerName = "reportCtrl";
+function ReportController(ReportService, CustomerService, MaintenanceService, $scope, $window, $http, QuickAlert, globalBaseUrl) {
+    var vm = this,
+        controllerName = "reportCtrl";
 
     vm.StartDate = "";
     vm.EndDate = "";
-    vm.ReportType = 0; // 0 - SalesReport; 1 - InventoryReport;
+    vm.ReportType = 0; // 0 - SalesReport; 1 - InventoryReport; 2 - SalesInventoryReport;
     vm.Report = {};
+    vm.ReportFilterLabel = "";
+    vm.ReportFilter = [];
+    vm.SelectedReportFilter = 0;
+    vm.SalesNo = 0;
+    vm.CategoryList = [];
+    vm.SelectedCategory = 0;
+    vm.CustomerList = [];
+    vm.SelectedCustomer = 0;
 
-    vm.GenerateReport = _generateReport;
-    vm.ResetFields = _resetFields;
-    vm.Initialize = _initialize;
+    $scope.$watch(
+        function() {
+            return vm.SelectedReportFilter;
+        },
+        function(oldValue, newValue) {
+            if (oldValue != newValue) {
+                vm.SalesNo = 0;
+                vm.SelectedCustomer = 0;
+                vm.SelectedCategory = 0;
+                vm.StartDate = "";
+                vm.EndDate = "";
+            }
+        }
+    );
 
-    function _initialize() {
+    vm.Initialize = function() {
         ReportService.InitializeReportPage().then(
-            function (data) {
-            }, function (err) {
+            function(data) {
+                vm.ReportFilterLabel = "--Select A Report Filter--";
+
+                vm.ReportFilter = [{
+                        FilterId: 1,
+                        FilterLabel: "By Sales No."
+                    },
+                    {
+                        FilterId: 2,
+                        FilterLabel: "By Customer"
+                    },
+                    {
+                        FilterId: 3,
+                        FilterLabel: "By Date"
+                    },
+                    {
+                        FilterId: 4,
+                        FilterLabel: "By Category and Date"
+                    },
+                ];
+
+                getCustomerList();
+                getCategoryList();
+            },
+            function(err) {
                 QuickAlert.Show({
                     type: 'error',
                     message: err
@@ -27,49 +70,108 @@ function ReportController(ReportService, $scope, $window, $http, QuickAlert, glo
             });
     }
 
-    function _generateReport() {
-
+    vm.GenerateReport = function() {
         var startDateString = vm.StartDate,
             endDateString = vm.EndDate,
             startDate = new Date(startDateString),
-            endDate = new Date(endDateString),
-            url = '';
+            endDate = new Date(endDateString);
 
-
-        if (vm.ReportType == 1) {
+        if (vm.ReportType !== 2 || (vm.ReportType === 2 && (vm.SelectedReportFilter === 3 || vm.SelectedReportFilter === 4))) {
             if (dateDiffDays(startDate, endDate) > 30) {
                 QuickAlert.Show({
                     type: 'error',
                     message: 'Date range can only be a maximum of 30 days.'
                 });
+                return;
             } else if ((startDate - endDate) > 0) {
                 QuickAlert.Show({
                     type: 'error',
                     message: 'End date should be after Start Date.'
                 });
-            } else {
-                url = globalBaseUrl + '/Report/GeneratePurchaseAndSalesReport?startDate=' + startDateString + '&endDate=' + endDateString;
-                downloadFile(url);
+                return;
             }
-        } else {
-            url = globalBaseUrl + '/Report/GenerateSalesReport';
-            downloadFile(url);
         }
 
-        
+        sendResponse();
     }
 
-    // Private Functions
-    function dateDiffDays(dt1, dt2) {
-        return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
-    }
-
-    function _resetFields() {
+    vm.ResetFields = function() {
         vm.StartDate = "";
         vm.EndDate = "";
     }
 
-    function downloadFile(url) {
+    vm.ValidReportParameters = function() {
+        switch (vm.ReportType) {
+            case 0:
+            case 1:
+                return vm.StartDate === "" || vm.EndDate === "" ? false : true
+            case 2:
+                switch (vm.SelectedReportFilter) {
+                    case 1:
+                        return vm.SalesNo === 0 || vm.SalesNo === '' ? false : true;
+                    case 2:
+                        return vm.SelectedCustomer === 0 ? false : true;
+                    case 3:
+                        return vm.StartDate === "" || vm.EndDate === "" ? false : true;
+                    case 4:
+                        return vm.StartDate === "" || vm.EndDate === "" || vm.SelectedCategory === 0 ? false : true
+                }
+        }
+    }
+
+    // Private Functions
+    sendResponse = function() {
+        switch (vm.ReportType) {
+            case 0:
+                url = globalBaseUrl + '/Report/GenerateSalesReport=' + vm.StartDate + '&endDate=' + vm.EndDate;
+                break;
+            case 1:
+                url = globalBaseUrl + '/Report/GeneratePurchaseAndSalesReport?startDate=' + vm.StartDate + '&endDate=' + vm.EndDate;
+                break;
+            case 2:
+                url = globalBaseUrl + '/Report/SalesOrderReportGeneration?' +
+                    '&SalesNo=' + vm.SalesNo +
+                    '&CustomerId=' + vm.SelectedCustomer +
+                    '&startDate=' + vm.StartDate +
+                    '&endDate=' + vm.EndDate +
+                    '&CategoryId=' + vm.SelectedCategory;
+                break;
+        }
+
+        downloadFile(url);
+    }
+
+    getCustomerList = function() {
+        CustomerService.GetCustomerList().then(
+            function(data) {
+                vm.CustomerList = data.CustomerDetailsResult;
+            },
+            function(error) {
+                alert(error);
+                QuickAlert.Show({
+                    type: 'error',
+                    message: 'Error fetching Customer List from Server.'
+                });
+            }
+        );
+    }
+
+    getCategoryList = function() {
+        MaintenanceService.GetCategoryList().then(
+            function(data) {
+                vm.CategoryList = data.CategoryDetailsResult;
+            },
+            function(error) {
+                vm.LoaderErrorMessage = error;
+            }
+        )
+    }
+
+    dateDiffDays = function(dt1, dt2) {
+        return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
+    }
+
+    downloadFile = function(url) {
         var a = document.createElement('a');
         a.href = url;
         a.target = '_blank';
